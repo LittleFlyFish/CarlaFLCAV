a6c44fa19e2362e49d7e31a8f203d80c587ebfe8
soe <buliyanran@gmail.com>
2023-10-24 15:48:15 +0100

    push

--- FLDatasetTool/label_tools/kitti_objects_label.py
+++ FLDatasetTool/label_tools/kitti_objects_label.py
@@ -20,8 +20,8 @@ def gather_rawdata_to_dataframe(record_name: str, vehicle_name: str, lidar_path:
     # vehicle_poses_df = load_vehicle_pose("{}/{}/{}".format(RAW_DATA_PATH, record_name, vehicle_name))
     # rawdata_frames_df = vehicle_poses_df
 
+    print('The data path is:', RAW_DATA_PATH, record_name)
     object_labels_path_df = load_object_labels("{}/{}/others.world_0".format(RAW_DATA_PATH, record_name))
-    print('The data path is:', object_labels_path_df)
     rawdata_frames_df = object_labels_path_df
     rawdata_frames_df = rawdata_frames_df.reset_index(drop=False)
 
