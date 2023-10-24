548df26bef97c3086828a0d88ccc041c737eba17
soe <buliyanran@gmail.com>
2023-10-24 10:43:58 +0100

    Camera Settings

---
+++ Images2Video.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+
+# Path to the folder containing the images
+folder_path = './FLYolo/raw_data/record_2023_1020_2254/vehicle.tesla.model3_13/image_2'
+
+# Get a list of all files in the folder
+files = os.listdir(folder_path)
+
+# Sort the files based on their names
+files.sort()
+
+# Get the first image file to extract its dimensions
+first_image = cv2.imread(os.path.join(folder_path, files[0]))
+height, width, _ = first_image.shape
+
+# Define the output video path and settings
+output_path = './FLYolo/raw_data/output_video.mp4'
+
+fps = 30
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video_writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+# Loop through each image file and write to the video
+for file in files:
+    image_path = os.path.join(folder_path, file)
+    image = cv2.imread(image_path)
+    video_writer.write(image)
+
+# Release the video writer and display a success message
+video_writer.release()
+print(f"Video created: {output_path}")
